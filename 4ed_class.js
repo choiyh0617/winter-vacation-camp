e58a2f17edc1d2142bc035747b10294e9332e84d
choiyh0617 <6l0mrc00057@gmail.com>
2025-01-09 11:39:11 +0900

    .

--- 4ed_class.js
+++ 4ed_class.js
@@ -1,12 +1,30 @@
-const myPromise = new Promise((resolve, reject)=>{
+// const myPromise = new Promise((resolve, reject)=>{
+//     const success = true;
+//     if (success) {
+//         resolve("작업 성공")
+//     } else{
+//         reject(new Error("작업 살패"))
+//     }
+// })
+// myPromise.then((result)=>{
+//     console.log("성공",result)
+// })
+// .catch((error)=>{
+//     console.error("실패",error.message)
+// })
+
+const myPromise = (number)=>
+new Promise((resolve, reject)=>{
     const success = true;
-    if (success) {
-        resolve("작업 성공")
+    if (number % 2 ===0) {
+        resolve(`${number}짝수`)
     } else{
-        reject(new Error("작업 살패"))
+        reject(new Error(`${number}홀수`))
     }
 })
-myPromise.then((result)=>{
+
+myPromise(4)
+.then((result)=>{
     console.log("성공",result)
 })
 .catch((error)=>{
